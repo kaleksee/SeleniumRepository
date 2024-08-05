b685d9ad4ea15497eecd178d8c1fb4eb66c5f0f0
Kseniia <xusha.alexeeva@mail.ru>
2024-08-05 20:20:10 +0300

    Removed unnecessary imports

--- src/main/java/com/herokuapp/theinternet/base/TestListener.java
+++ src/main/java/com/herokuapp/theinternet/base/TestListener.java
@@ -1,13 +1,10 @@
 package com.herokuapp.theinternet.base;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
-import java.io.File;
 
 public class TestListener implements ITestListener  {
 
